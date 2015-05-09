0f1b5f81c2bd47b7b9cc3a41c9ad4281ba5aba5e
karupanerura <karupa@cpan.org>
2015-05-09 18:02:44 +0900

    delayed echo!

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,5 +1,40 @@
+import akka.actor.{Actor,ActorSystem,Props}
+import akka.pattern.ask
+import akka.pattern.gracefulStop
+import akka.util.Timeout
+import scala.concurrent.Future
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class DelayEchoActor extends Actor {
+  def receive = {
+    case msg: String =>
+      Thread.sleep(1000)
+      sender ! msg
+  }
+}
+
 object HelloWorld {
   def main(args: Array[String]) = {
-    println("Hi!")
+    var sys = ActorSystem("system")
+    val echo = sys.actorOf(Props[DelayEchoActor])
+
+    implicit val timeout = Timeout(10 seconds)
+
+    val futures = scala.collection.mutable.ArrayBuffer.empty[Future[Any]]
+    futures += echo ? "あれ？"
+    futures += echo ? "文字列が"
+    futures += echo ? "遅れて"
+    futures += echo ? "流れて"
+    futures += echo ? "くるよ？"
+
+    for (future <- futures.toArray) {
+      val res = Await.result(future, timeout.duration)
+      res match {
+        case msg: String => println(msg)
+      }
+    }
+
+    sys.shutdown()
   }
 }
